80c5b706b8f3d814081d1c86e2c6fcd05111e3cf
Yash Chaudhary <124380387+YashChaudhary16@users.noreply.github.com>
2024-09-13 03:12:57 +0000

    updated test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ array2 = [18,2,23,21,15,9,110]
 array3 = [81,62,23,44,15,93,10]
 
 def testcase1():
-  assert hw2_debugging.mergeSort(array1) == array1.sort()
+  assert hw2_debugging.mergeSort(array1) == [1,2,4,6,8,9,10]
 def testcase2():
-  assert hw2_debugging.mergeSort(array2) == array2.sort()
+  assert hw2_debugging.mergeSort(array2) == [2,9,15,18,21,23,110]
 def testcase3():
-  assert hw2_debugging.mergeSort(array3) == array3.sort()
+  assert hw2_debugging.mergeSort(array3) == [10,15,23,44,62,81,93]
